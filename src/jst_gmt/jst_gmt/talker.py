41fbee2c2c15862f740cea21fe1fb36abff19e42
Yuki Nishijima <s23C1101EC@s.chibakoudai.jp>
2025-01-20 23:11:43 +0900

    .

--- src/jst_gmt/jst_gmt/talker.py
+++ src/jst_gmt/jst_gmt/talker.py
@@ -3,7 +3,7 @@ from rclpy.node import Node
 from std_msgs.msg import Int16
 
 rclpy.init()
-node = Node("talker")
+node = Node("talker")   
 pub = node.create_publisher(Int16, "countup", 10)
 n = 0
 
