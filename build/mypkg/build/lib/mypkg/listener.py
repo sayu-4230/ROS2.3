d200962174f373b0910fd806b37e6c8a95bf3543
Yuki Nishijima <s23C1101EC@s.chibakoudai.jp>
2025-01-21 20:33:11 +0900

    .

--- build/mypkg/build/lib/mypkg/listener.py
+++ build/mypkg/build/lib/mypkg/listener.py
@@ -7,11 +7,26 @@ rclpy.init()
 node = Node("listener")
 
 
-def cb(msg):
-    global node
-    node.get_logger().info("Listen: %d" % msg.data)
-
-
 def main():
-    pub = node.create_subscription(Int16, "countup", cb, 10)
-    rclpy.spin(node)
+    client = node.create_client(Query, 'query')
+    while not client.wait_for_service(timeout_sec=1.0):
+        node.get_logger().info('待機中')
+
+    req = Query.Request()
+    req.name = "上田隆一"
+    future = client.call_async(req)
+
+    while rclpy.ok():
+        rclpy.spin_once(node)
+        if future.done():
+            try:
+                response = future.result()
+            except:
+                node.get_logger().info('呼び出し失敗')
+            else:
+                node.get_logger().info("age: {}".format(response.age))
+
+            break
+
+    node.destroy_node()
+    rclpy.shutdown()
--- build/mypkg/build/lib/mypkg/talker.py
+++ build/mypkg/build/lib/mypkg/talker.py
@@ -1,22 +1,21 @@
 import rclpy
 from rclpy.node import Node
-from person_msgs.msg import Person
+from person_msgs.msg import Int16
 
-rclpy.init()
-node = Node("talker")
-pub = node.create_publisher(Person, "person", 10)
-n = 0
-
-
-def cb():
-    global n
-    msg = Person()
-    msg.name = "上田隆一"
-    msg.age = n
-    pub.publish(msg)
-    n += 1
+class Talker(Node):
+    def __init__(self):
+        super().__init__("takler")
+        self.pub = self.create_publisher(Int16, "countup", 10)
+        self.create_timer(0.5, self.cb)
+        self.n = 0
+    def cb(self):
+        msg = Int16()
+        msg.data = self.n
+        self.pub.publish(msg)
+        self.n += 1
 
 
 def main():
-    node.create_timer(0.5, cb)
+    rclpy.init()
+    node = Talker()
     rclpy.spin(node)
